28bc025be1fc51d4aac5f5990131ad2d9a88b81e
Mariusz Maciejewski <mariusz.maciejewski1@onet.pl>
2025-01-09 20:51:23 +0100

    initial commit

---
+++ message.py
@@ -0,0 +1,20 @@
+
+# Email
+
+sender = "MarioSantana@op.pl"
+recipient = "DawidZaba@wp.pl"
+title = "We learn the Python programming language"
+attachment = "https://github.com/MarioSantana93"
+messageContent = """Hey tiger, how's your Python learning going?
+In the attachment I am sending you my profile created on gitHub"""
+postscriptum = "don't forget to take modafinil and drink boar :)"
+
+conversationBetween = sender + " from to " + recipient
+print(conversationBetween)
+
+titleAndValues = title + "\n" + attachment
+print(titleAndValues)
+
+messageAndForgetful = messageContent + "\n" + postscriptum
+print(messageAndForgetful)
+
